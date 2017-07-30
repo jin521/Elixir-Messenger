2c7712bf16960d232eb44ec0936fa5ea73a3e2ad
Qiu <jin.qiu@marketboomer.com>
2017-07-30 16:36:35 +1000

    first commit

---
+++ web/channels/random_channel.ex
@@ -0,0 +1,36 @@
+defmodule Slackir.RandomChannel do
+  use Slackir.Web, :channel
+
+  def join("elixirbridge", payload, socket) do
+    if authorized?(payload) do
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (random:lobby).
+  def handle_in("shout", payload, socket) do
+    broadcast socket, "shout", payload
+    {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
+  end
+
+  def handle_in("new_message", message, socket) do
+    Slackir.Message.changeset(%Slackir.Message{}, message)
+      |> Slackir.Repo.insert
+    broadcast! socket, "new_message", message
+    {:noreply, socket}
+  end
+end
